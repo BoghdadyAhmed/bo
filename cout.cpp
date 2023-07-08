6fa72b0b4142108a78077ddbae3294ee13919a85
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2023-07-08 10:56:02 +0000

    cour code

--- cout.cpp
+++ cout.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
+    string name;
+    cout << "hallo" << endl;
 
-cout <<"hallo"<<endl;
-
-cout << "what is your name\n";
-
-
+    cout << "what is your name\n";
+    cin >> name;
+    cout << "your name is " << name << endl;
+    
 
 
     return 0;
