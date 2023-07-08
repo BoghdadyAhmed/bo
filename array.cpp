d9f6c0794edd23aed7d9e8fb647f0ee0b89ce6a0
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2023-07-08 17:32:00 +0000

    cpp code

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "enter tow number " << endl;
+    cin >> x >> y;
+    cout << "sum of tow numbers=" << x + y << endl;
+    return 0;
+}
