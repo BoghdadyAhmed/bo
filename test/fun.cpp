a0e26153852aef1dc648876d666500cddb045e3e
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2023-07-09 09:15:44 +0000

    overloaded functions

---
+++ test/fun.cpp
@@ -0,0 +1,31 @@
+ 
+ // overloaded function
+ // is a normal functions but هي اكثر من داله بنفس الاسم ولاكن ب ترجع نوع بينات مختلف 
+  
+ #include <iostream>
+using namespace std;
+
+void print()
+{
+    cout << "Hello World!" << endl;
+}
+
+void print(int a)
+{
+
+    cout << "integer you entered: " << a << endl;
+}
+void print(int a, int b)
+{
+
+    cout << "sum of two numbers is " << a + b << endl;
+}
+int main()
+{
+
+    print();
+    print(10);
+    print(10, 15);
+
+    return 0;
+}
